9fcadbc13184f309cd92afb297af95df55a5c5f2
John J. Lawal <jophilip79@gmail.com>
2023-12-13 13:31:37 +0100

    Edited database settings for production

--- cares/production.py
+++ cares/production.py
@@ -32,6 +32,7 @@ DATABASES = {
         'NAME': os.environ['DBNAME'],
         'HOST': hostname + ".postgres.database.azure.com",
         'USER': os.environ['DBUSER'] + "@" + hostname,
-        'PASSWORD': os.environ['DBPASS'] 
+        'PASSWORD': os.environ['DBPASS'],
+        'OPTIONS': {'sslmode': 'require'} 
     }
 }
